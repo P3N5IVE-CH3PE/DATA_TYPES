e76ebaea1901360b00fb3c68d5d0f5a1b80e9557
P3N5IVE_CH3PE <P3N5IVE_CH3PE@protonmail.com>
2024-09-10 14:15:52 -0700

    Adding different data types. Such as tuples, arrays...

--- DATA_TYPES/src/main.rs
+++ DATA_TYPES/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let x = 2.0; //f64
     let y: f32 = 3.0; // f32
@@ -26,4 +28,39 @@ fn main() {
     let c = 'z';
     let z: char = 'Z';
     let heart_eyes_cat = '😻';
+
+				let tup: (i32, f64, u8) = (500, 6.1, 2);
+				
+				let (x, y, z) = tup;
+				
+				println!("The value of y is: {y}");
+
+
+
+				let tups: (i32, f64, u8) = (100,3.14, 42);
+				let five_hundred = tups.0;
+				let pie = tups.1;
+				let meaning_of_life = tups.2;
+
+				let a = [1,2,3,4,5];
+	
+				println!("Please enter an array index.");
+
+				let mut index = String::new();
+				
+				io::stdin()
+					.read_line(&mut index)
+					.expect("Failed to read line");
+
+				let index: usize = index
+					.trim()
+					.parse()
+					.expect("Index entered was not a number");
+
+				let element = a[index];
+
+				println!("The value of the element at index {index} is: {element}");
+
+				
+
 }
